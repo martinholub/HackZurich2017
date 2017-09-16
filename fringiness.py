c8379f3773a123899ba4353fa925a7b441b9354c
skerst <stan.kerstjens@me.com>
2017-09-16 11:00:22 +0200

    Start bokeh plot

--- fringiness.py
+++ fringiness.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from sklearn.decomposition import PCA
 
+import matplotlib.pyplot as plt
+
 def fringiness(data, distance_metric='cosine'):
     """Calculates the fringiness of news articles.
 
@@ -42,3 +44,34 @@ def fringiness(data, distance_metric='cosine'):
     sum_sq_loadings /= sum_sq_loadings.max()
     return loadings.T[0], loadings.T[1], sum_sq_loadings
 
+import json
+
+def to_json(x, y, s, filename=None):
+    d = [{'x':xx,'y':yy,'s':ss} for xx, yy, ss in zip(x, y, s)]
+    if filename is not None:
+        with open(filename,'w') as f:
+            json.dump(d, f)
+    return d
+
+
+def embedding_plot_mpl(x, y, s):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x, y, c=s, vmin=0, vmax=1)
+    ax.axis('off')
+    return fig
+
+from bokeh.plotting import figure, show
+from bokeh.palettes import viridis
+
+def embedding_plot_bokeh(x, y, s):
+    col_ind = np.digitize(s, np.linspace(s.min(), s.max(), 20))
+    colormap = viridis(21)
+    colors = [colormap[int(ind)] for ind in col_ind]
+    colors[0] = 'red'
+
+    p = figure(title="Article embedding", tools=['tap','pan','zoom_in',
+        'zoom_out','wheel_zoom'])
+    p.circle(x, y, fill_color=colors, line_color=colors)
+
+    return p
